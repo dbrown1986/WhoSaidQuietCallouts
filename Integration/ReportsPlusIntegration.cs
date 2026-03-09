727f358eda44dd2e26a7c4b2e8423950518764ee
dbrown1986 <dbrown1986@hotmail.com>
2026-03-08 18:22:31 -0700

    Update ReportsPlusIntegration.cs

--- Integration/ReportsPlusIntegration.cs
+++ Integration/ReportsPlusIntegration.cs
@@ -5,7 +5,7 @@ namespace WhoSaidQuietCallouts.Core
 {
     /// <summary>
     /// ReportsPlusIntegration.cs
-    /// Version: 1.9.1 (Maintenance & Documentation Cleanup Build)
+    /// Version: 0.9.1 Alpha (Maintenance & Documentation Cleanup Build)
     /// Date: March 7, 2026
     /// Author: Who Said Quiet Team
     ///
